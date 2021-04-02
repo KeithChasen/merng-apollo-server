4b77aa26948075cd6229e3a6c42a4d4d7dcf88ce
keithchasen <keithchasen89@gmail.com>
2021-04-02 23:32:16 +0300

    getting the post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -8,6 +8,18 @@ module.exports = {
       } catch (e) {
         throw new Error(e)
       }
+    },
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          return post;
+        } else {
+          throw new Error('Post not found');
+        }
+      } catch (error) {
+        throw new Error(error);
+      }
     }
   }
 };
